453690138294011e9d1695ffd32aca19e1e34cbb
Yuvraj Singh <116381444+yuvr0131@users.noreply.github.com>
2026-08-20 23:42:07 +0530

    Add files via upload

---
+++ variables_data_types/app.py
@@ -0,0 +1,9 @@
+# variables and data types
+character_name="John"
+character_age=35
+print(f"There was a man name { character_name},")
+print(f"he was  {character_age}  years old.")
+character_name="Tom"
+print(f"He really liked the name {character_name},")
+print(f"but didn't like being {character_age}.")
+
