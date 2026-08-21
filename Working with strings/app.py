550f8cf5b623636375adf5f26cf399bb2f928e21
Yuvraj Singh <116381444+yuvr0131@users.noreply.github.com>
2026-08-21 11:33:33 +0530

    Add files via upload

---
+++ Working with strings/app.py
@@ -0,0 +1,10 @@
+
+#working with strings
+phrase="Giraffe  Academy"
+print(phrase.lower().islower())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("A"))
+print(phrase.replace("Giraffe","Elephant"))
+
+
