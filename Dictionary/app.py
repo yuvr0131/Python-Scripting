0bab664dbc70f1587817deb5ba2b1b147609b169
Yuvraj Singh <116381444+yuvr0131@users.noreply.github.com>
2026-08-21 16:35:01 +0530

    Add files via upload

---
+++ Dictionary/app.py
@@ -0,0 +1,18 @@
+#dictionary - {key:value}-unique key
+monthConversions={
+    "Jan": "January",
+    "Feb":"February",
+    "Mar":"March",
+    "Apr":"April",
+    "May":"May",
+    "Jun":"June",
+    "Jul":"July",
+    "Aug":"August",
+    "Sep":"September",
+    "Oct":"October",
+    "Nov":"November",
+    "Dec":"December"
+}
+
+print(monthConversions["Mar"])
+print(monthConversions.get("Dec"))
