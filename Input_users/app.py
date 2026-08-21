265a89e25c3ff38fa84cac7d2b082d08ee8191ca
Yuvraj Singh <116381444+yuvr0131@users.noreply.github.com>
2026-08-21 12:21:01 +0530

    Add files via upload

---
+++ Input_users/app.py
@@ -0,0 +1,23 @@
+# getting input from users
+
+"""name= input(" Enter your name:")
+age= input(" Enter your age:")
+print("Hello" + name + "! You are" + age)"""
+
+#building a basic calculator
+# by default python takes input as a string, so we have to convert it into a number
+"""num1= input("Enter a number:")
+num2=input("Enter another number:")
+result= float(num1)+float(num2)
+print(result)"""
+
+#Mad Libs Game
+color=input("Enter a color:")
+plural_noun=input("Enter a plural noun:")
+celebrity=input("Enter a celebrity:")
+print("Roses are" +color)
+print(plural_noun+" are blue")
+print("I love" + celebrity)
+
+
+
