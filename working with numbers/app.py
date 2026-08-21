1fb6a5e6afa29650c244fa47dfc26a445c612814
Yuvraj Singh <116381444+yuvr0131@users.noreply.github.com>
2026-08-21 11:48:49 +0530

    Add files via upload

---
+++ working with numbers/app.py
@@ -0,0 +1,47 @@
+# working with numbers
+"""print(-2.0987)
+print(3+4.4)
+print(3*4+8)
+print((4+9)*7)
+print(10%3)
+my_num=8
+print(my_num)
+
+#converting number to string
+print(str(my_num)+" my favorite number")"""
+
+#function - abs
+"""num=-3
+print(abs(num))
+num=32.98
+print(abs(num))"""
+
+#function - pow()
+"""num=-5
+print(abs(pow(num,3)))
+
+#function - max()
+num=8
+print(max(num,2))"""
+
+# function- round
+"""print(round(2.9))"""
+
+# for accessing math functions
+from math import *
+print(floor(3.7))
+print(ceil(3.2))
+print(sqrt(25))
+
+
+
+
+
+
+
+
+
+
+
+
+
