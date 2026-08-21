1ce2dcea69f24877eeb97944d863afa5a183d0e3
Yuvraj Singh <116381444+yuvr0131@users.noreply.github.com>
2026-08-21 15:30:47 +0530

    Add files via upload

---
+++ Function/app.py
@@ -0,0 +1,19 @@
+#Functions
+#define function
+"""def say_hi(name,age):
+    print("hello " + name +", you are " + str(age))
+
+
+print("Top")
+#calling function
+say_hi("Yuvraj",23)
+say_hi("Mike",12)
+print("Bottom")"""
+
+#return statement
+#from math import pow
+def cube(num1,num2):
+    return pow(num1,num2)
+
+ans=cube(4,3)
+print(ans)
