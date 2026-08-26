116ea10536681efddb9f08c34490277d485ed72c
Yuvraj Singh <116381444+yuvr0131@users.noreply.github.com>
2026-08-26 13:00:49 +0530

    Add files via upload

---
+++ Classes_Objects/app.py
@@ -0,0 +1,6 @@
+#Classes and Objects
+from student import Student
+student1=Student("jim","Business",3.1,False)
+student2=Student("Pam","Art",4.2,True)
+print(student1.gpa)
+print(student2.is_on_probation)
---
+++ Classes_Objects/student.py
@@ -0,0 +1,6 @@
+class Student:
+    def __init__(self,name,major,gpa,is_on_probation):
+        self.name=name   #self.name is the object or attribute and we are passing the value in it.
+        self.major=major
+        self.gpa=gpa
+        self.is_on_probation=is_on_probation
