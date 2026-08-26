6594e40ca9ae6c1cac6c2b3b63220f47a0cc9583
Yuvraj Singh <116381444+yuvr0131@users.noreply.github.com>
2026-08-26 12:38:29 +0530

    Add files via upload

---
+++ Modules_PIP/app.py
@@ -0,0 +1,22 @@
+#Writing appending files
+# employee_file=open("emplyee.txt","r")
+# employee_file=open("emplyee.txt","a")
+# employee_file.write("TOBBY - Human Resources")
+# # print(employee_file.read())
+# employee_file=open("emplyee.txt","w")
+# employee_file.write("\n Kelly - Customer Service")
+# employee_file.close()
+
+# Modules and Pip
+#Modules - are python files which contain useful python functions which we require in different programs
+# import useful_tools
+# print(useful_tools.roll_dice(10))
+#pip- can be used to install python modules, it is used as a package manager(which can be used to install, manage,update,uninstall different python modules
+
+# import docx
+# docx.
+
+
+
+
+
